3d2e00d55ca643f6030955ad54138508665bf559
João Faro <joaobremgartner@gmail.com>
2017-03-05 21:50:03 -0300

    Adicionando activity de informacoes do app e alguns refactores

--- app/src/main/java/br/eng/joaofaro/ireserv/activities/ReservasActivity.java
+++ app/src/main/java/br/eng/joaofaro/ireserv/activities/ReservasActivity.java
@@ -1,4 +1,4 @@
-package br.eng.joaofaro.ireserv;
+package br.eng.joaofaro.ireserv.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import br.eng.joaofaro.ireserv.R;
 
 public class ReservasActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
