acdc2085e65d84b59b6cab4edd140b5a8212a37b
João Faro <joaobremgartner@gmail.com>
2017-03-05 23:18:56 -0300

    Ajustes no projeto para comecar o App

--- app/src/main/java/br/eng/joaofaro/ireserv/activities/InformacoesActivity.java
+++ app/src/main/java/br/eng/joaofaro/ireserv/activities/InformacoesActivity.java
@@ -22,7 +22,7 @@ public class InformacoesActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, R.string.emailireserv, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
--- app/src/main/java/br/eng/joaofaro/ireserv/activities/ReservasActivity.java
+++ app/src/main/java/br/eng/joaofaro/ireserv/activities/ReservasActivity.java
@@ -1,5 +1,6 @@
 package br.eng.joaofaro.ireserv.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import br.eng.joaofaro.ireserv.R;
 
@@ -29,7 +31,7 @@ public class ReservasActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, R.string.emailireserv, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -63,14 +65,14 @@ public class ReservasActivity extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.info:
+                Intent irParaInfo = new Intent(this, InformacoesActivity.class);
+                startActivity(irParaInfo);
+                Toast.makeText(this, "teste informações", Toast.LENGTH_LONG).show();
+                break;
         }
 
         return super.onOptionsItemSelected(item);
